607374950f4d4d47a7d97ec4272bc8596e997cca
Carlos Barrios <carlosbarriosf6@gmail.com>
2024-05-21 16:07:22 -0300

    added middlewares to products route

--- src/middlewares/validateResponse.js
+++ src/middlewares/validateResponse.js
@@ -1,4 +1,7 @@
 import { validationResult } from "express-validator";
+import fs from "fs"
+import path from "path";
+
 
 export const validationErrorResponse = (req, res, next) => {
     const errors = validationResult(req);
@@ -7,6 +10,27 @@ export const validationErrorResponse = (req, res, next) => {
         const errorList = errors
             .array()
             .map( err => ({[err.path]: err.msg}))
+
+        
+        const directoryPath = './temp/imgs';
+
+        try {
+            const files = fs.readdirSync(directoryPath);
+    
+            files.forEach(file => {
+            if (path.extname(file).toLowerCase() === '.png') {
+                try {
+                fs.unlinkSync(path.join(directoryPath, file));
+                console.log(`Deleted file: ${file}`);
+                } catch (err) {
+                console.log(`Error deleting file: ${file}`);
+                }
+            }
+            });
+        } catch (err) {
+            console.log(`Unable to scan directory: ${err}`);
+        }  
+            
     return res.status(400).json({
         ok: false,
         errors: errorList
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,14 +1,171 @@
 import express from "express"
 import { createProduct, deleteProductById, editProductById, getProductById, getProducts } from "../controllers/productController.js";
 import { upload } from "../libs/storage.js";
+import { body, param } from "express-validator";
+import {  validationErrorResponse } from "../middlewares/validateResponse.js";
+
 
 const route = express.Router();
 
+console.log(upload)
+
 route
-    .post("/", upload.single("imageFile"), createProduct)
+    .post(
+        "/", 
+        [
+            upload.single("imageFile"),
+            body('name')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('image')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string'),
+            body('price')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isFloat({min:0.01})
+                .withMessage('El valor del campo debe ser un número mayor a 0'),    
+            body('shortDesc')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:15, max: 60})
+                .withMessage('El campo debe contener entre 15 y 60 caracteres'),
+            body('longDesc')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string'),
+            body('ageFrom')
+                .optional()
+                .isInt()
+                .withMessage('El valor del campo debe ser un número entero'),
+            body('ageTo')
+                .optional()
+                .isInt()
+                .withMessage('El valor del campo debe ser un número entero'),
+            body('brand')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('category')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('freeShipping')
+                .optional()
+                .isBoolean()
+                .withMessage('El valor del campo debe ser del tipo booleano'),
+            body('stock')
+                .exists()
+                .withMessage('El campo es requerido')
+                .bail()
+                .isInt({min: 1})
+                .withMessage('El valor del campo debe ser un número mayor o igual a 1'),
+            validationErrorResponse
+        ], 
+        createProduct
+    )
     .get("/", getProducts)
-    .get("/:id", getProductById)
-    .put("/edit/:id", upload.single("imageFile"), editProductById)
-    .delete("/delete/:id", deleteProductById)
+    .get(
+        "/:id",
+        [
+            param('id').isMongoId().withMessage('El formato del id no es correcto'),
+            validationErrorResponse
+        ],
+        getProductById
+        )
+    .put(
+        "/edit/:id",
+        [
+            upload.single("imageFile"),
+            param('id').isMongoId().withMessage('El formato del id no es correcto'),
+            body('name')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('image')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string'),
+            body('price')
+                .optional()
+                .isFloat({min:0.01})
+                .withMessage('El valor del campo debe ser un número mayor a 0'),    
+            body('shortDesc')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:15, max: 60})
+                .withMessage('El campo debe contener entre 15 y 60 caracteres'),
+            body('longDesc')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string'),
+            body('ageFrom')
+                .optional()
+                .isInt()
+                .withMessage('El valor del campo debe ser un número entero'),
+            body('ageTo')
+                .optional()
+                .isInt()
+                .withMessage('El valor del campo debe ser un número entero'),
+            body('brand')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('category')
+                .optional()
+                .isString()
+                .withMessage('El valor del campo debe ser del tipo string')
+                .bail()
+                .isLength({min:3, max: 30})
+                .withMessage('El campo debe contener entre 3 y 30 caracteres'),
+            body('freeShipping')
+                .optional()
+                .isBoolean()
+                .withMessage('El valor del campo debe ser del tipo booleano'),
+            body('stock')
+                .optional()
+                .isInt({min: 1})
+                .withMessage('El valor del campo debe ser un número mayor o igual a 1'),
+            validationErrorResponse
+        ], 
+        editProductById)
+    .delete(
+        "/delete/:id",
+        [
+            param('id').isMongoId().withMessage('El formato del id no es correcto'),
+            validationErrorResponse
+        ],
+        deleteProductById
+        )
 
 export default route;
